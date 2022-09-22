1a10869e9b99324d2315300977b3baef8ddbcd5c
kaubu <kaution.burrowes@gmail.com>
2022-09-23 06:21:06 +1200

    First changes

---
+++ target/debug/deps/lazy_static-51f8aa49f78e53b1.d
@@ -0,0 +1,8 @@
+/run/media/me/Games/Documents/Coding/Rust/NOT_MINE/rust_lemmatizer2/target/debug/deps/lazy_static-51f8aa49f78e53b1.rmeta: /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/run/media/me/Games/Documents/Coding/Rust/NOT_MINE/rust_lemmatizer2/target/debug/deps/liblazy_static-51f8aa49f78e53b1.rlib: /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/run/media/me/Games/Documents/Coding/Rust/NOT_MINE/rust_lemmatizer2/target/debug/deps/lazy_static-51f8aa49f78e53b1.d: /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs
+
+/home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/lib.rs:
+/home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy_static-1.4.0/src/inline_lazy.rs:
---
+++ target/debug/librust_lemmatizer.d
@@ -0,0 +1,1 @@
+/run/media/me/Games/Documents/Coding/Rust/NOT_MINE/rust_lemmatizer2/target/debug/librust_lemmatizer.rlib: /run/media/me/Games/Documents/Coding/Rust/NOT_MINE/rust_lemmatizer2/src/lib.rs
